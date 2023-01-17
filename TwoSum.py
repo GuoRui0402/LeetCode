2237b567e2330b00ab77892cea248d38ff3485f7
GuoRui0402 <heguorui0402@gmail.com>
2023-01-17 23:59:56 +0800

    updata TwoSum python3

--- TwoSum.py
+++ TwoSum.py
@@ -1,14 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        ans = []
+        numdict = {}
         for i in range(0,n):
-            for j in range(i+1,n):
-                tmp = nums[i]+nums[j]
-                if tmp == target:
-                    ans.append(i)
-                    ans.append(j)
-                    break
-        
-
-        return ans
+            if (target - nums[i]) in numdict:
+                return [ i , numdict[ target - nums[i] ] ]
+            else:
+                numdict[ nums[i] ] = i
